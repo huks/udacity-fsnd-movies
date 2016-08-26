83f75e0dbce1bbe3303b021b0a69409538967758
Byung Joo Kim <huks7417@gmail.com>
2016-08-26 13:58:05 +0900

    Comments added

--- media.py
+++ media.py
@@ -1,19 +1,35 @@
+# -*- coding: utf-8 -*-
+
 import webbrowser
 
 class Video():
+    """
+    Parent Class
+    """
     def __init__(self, title, runtime):
-        #print("Video Constructor Called")
+        """
+        Create an instance of Video with tile and runtime proprties
+        """
         self.title = title
         self.runtime = runtime
 
 class Movie(Video):
+    """
+    Child class of Video
+    """
     def __init__(self, movie_title, movie_duration,
                  movie_storyline, poster_image, trailer_youtube):
-        #print("Movie Constructor Called")
+        """
+        Create an instance of Movie with proprties:
+        title, duration, storyline, poster, trailer
+        """
         Video.__init__(self, movie_title, movie_duration)
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url= trailer_youtube
 
     def show_trailer(self):
+        """
+        Open youtube trailer
+        ""
         webbrowser.open(self.trailer_youtube_url)
